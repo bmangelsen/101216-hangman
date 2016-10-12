52cabb65723c8e1368218e2d166c5bc25f95ca3b
Benjamin Mangelsen <bmangelsen@lmao.local>
2016-10-12 13:51:29 -0400

    I established my word list and began my while loop which will run once for each game of hangman

---
+++ hangman.rb
@@ -0,0 +1,19 @@
+word_list=["vulture", "toucan", "moose", "elephant", "alpaca", "alligator",
+  "chimpanzee", "dolphin", "tarantula", "sparrow", "wallaby", "wombat"]
+
+play_again = "y"
+
+while play_again == "y"
+
+  chances = 0
+  hangman_word = word_list.sample
+
+  puts "Guess a letter! You get eight chances"
+  user_guess = gets.chomp
+
+
+  #method to test if valid letter
+
+  #method to display current board
+
+  #method to
